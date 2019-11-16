e21ddf8ec3ce4b42fb39a3f82e91f86a48824b89
B Seamus Haney <43055154+Bhaney44@users.noreply.github.com>
2019-11-16 13:46:42 -0500

    Add files via upload

---
+++ Remainders.py
@@ -0,0 +1,15 @@
+#Remainders
+#In Python % is a modulus
+    #Returns the remainder when the first operancd is divided by the second
+
+u = 17 % 6
+print(u)
+
+p = 12 % 6
+print(p)
+
+r = 18 % 6
+print(r)
+
+v = 9 % 6
+print(v)
